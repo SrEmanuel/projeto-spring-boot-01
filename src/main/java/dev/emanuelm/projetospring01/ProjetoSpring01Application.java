4ae40e1bfcee00836ac1a6786a861e7c7693d3f1
Emanuel Martins <emanuelmartins284@gmail.com>
2022-01-17 14:48:19 -0300

    Project created

---
+++ src/main/java/dev/emanuelm/projetospring01/ProjetoSpring01Application.java
@@ -0,0 +1,13 @@
+package dev.emanuelm.projetospring01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpring01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpring01Application.class, args);
+    }
+
+}
