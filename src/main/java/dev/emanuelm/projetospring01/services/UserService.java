341b90ca7ef672c0e1f9dd5e515febe997b03b3e
Emanuel Martins <emanuelmartins284@gmail.com>
2022-01-20 15:00:26 -0300

    Exception handling  - find by id

--- src/main/java/dev/emanuelm/projetospring01/services/UserService.java
+++ src/main/java/dev/emanuelm/projetospring01/services/UserService.java
@@ -2,6 +2,7 @@ package dev.emanuelm.projetospring01.services;
 
 import dev.emanuelm.projetospring01.entities.User;
 import dev.emanuelm.projetospring01.repositories.UserRepository;
+import dev.emanuelm.projetospring01.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User inser(User obj){
---
+++ src/main/java/dev/emanuelm/projetospring01/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package dev.emanuelm.projetospring01.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id: " + id);
+    }
+
+
+}
